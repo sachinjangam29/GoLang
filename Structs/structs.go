d42aee4b549486d573c81d0443a7a0c1d1a160e0
sachin jangam <acejangam2910@gmail.com>
2024-06-24 00:10:52 +0530

    added new function returing a new user with the pointer

--- Structs/structs.go
+++ Structs/structs.go
@@ -12,8 +12,26 @@ type user struct {
 	createdAt time.Time
 }
 
-func (u user) oututUser() {
+// * is used with the user type with a function to refer to the same data, if we do not use
+// * then this function will refer to a new copy of the struct data which will give new data and it will not
+// refer to the same data.
+func (u *user) oututUser() {
 	fmt.Println(u.firstName, u.lastName, u.birthDate, u.createdAt)
+}
+
+func (u *user) clearUser() {
+	u.firstName = ""
+	u.lastName = ""
+	u.birthDate = ""
+}
+
+func newUser(firstName, lastName, birthDate string) *user {
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}
 }
 
 func main() {
@@ -21,13 +39,12 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthDate := getUserData("Please enter your DOB (MM/DD/YYYY): ")
 
-	var appUser user
-	appUser = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: userBirthDate,
-		createdAt: time.Now(),
-	}
+	var appUser *user
+
+	appUser = newUser(userFirstName, userLastName, userBirthDate)
+
+	appUser.oututUser()
+	appUser.clearUser()
 	appUser.oututUser()
 }
 
